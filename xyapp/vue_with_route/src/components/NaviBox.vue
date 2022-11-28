71883d9522f87227a000522ee995eb34d1de8c17
Orbit-y <2665907790@qq.com>
2022-11-28 10:12:34 +0800

    增加了部分页面吸顶和吸底功能

--- xyapp/vue_with_route/src/components/NaviBox.vue
+++ xyapp/vue_with_route/src/components/NaviBox.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <router-link to='/FirstPage'>
+  <div className="nav">
+    <router-link to='/FirstPage' className="nav-item">
       <img style="margin-left: 0px; margin-top: 200px;" src="@/assets/多选.svg">
     </router-link>
-    <router-link to='/PostPage'>
+    <router-link to='/PostPage' className="nav-item">
       <img style="margin-left: 90px; margin-top: 200px;" src="@/assets/plus.svg">
     </router-link>
-    <router-link to='/InfoList'>
+    <router-link to='/InfoList' className="nav-item">
       <img style="margin-left: 90px; margin-top: 200px;" src="@/assets/message-circle.svg">
     </router-link>
   </div>
@@ -17,3 +17,36 @@ export default {
   name: "NaviBox",
 }
 </script>
+<style scoped>
+.nav {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: space-between;
+  width: 328px;
+  height: 50px;
+  overflow: hidden;
+  margin: 0 auto;
+  background: #FFFFFF;
+}
+
+.nav-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  overflow: hidden;
+  margin: 0 32px;
+}
+
+.nav-item img {
+  width: 32px;
+  height: 32px;
+  margin: 0 !important;
+  padding: 0 !important;
+}
+
+</style>
