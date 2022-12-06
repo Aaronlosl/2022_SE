baea5af2be80cb6ab68a86285080039296a9aa5d
hzc1208 <60738150+hzc1208@users.noreply.github.com>
2022-12-06 08:36:49 +0800

    增加用户信息更新函数

--- xyapp/jsonresponse/urls.py
+++ xyapp/jsonresponse/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
     path('advpost/', views.receive_ad_post, name='advpost'),
     path('markpost/', views.mark_ad_post, name='markpost'),
     path('showpost/', views.show_ad_post, name='showpost'),
-    path('showUserInfo/', views.showUserInfo, name='showUserInfo')
+    path('showUserInfo/', views.showUserInfo, name='showUserInfo'),
+    path('update_userinfo/', views.update_userinfo, name='update_userinfo')
 ]
